074a86932e02046bbbfb5b087a00fd73729e4fda
R4n7ala <toni.rantala@live.fi>
2021-05-30 23:17:13 +0300

    ...

--- scripts.js
+++ scripts.js
@@ -1,37 +1,9 @@
 window.onscroll = function(e){
     if(e.target.scrollingElement.scrollTop > 10) {
-         $('body').addClass('nav-smaller')
+         $('body').addClass('nav-smaller');
              
     } else {
-		$('body').removeClass('nav-smaller')
+		$('body').removeClass('nav-smaller');
     }
 };
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("myslideshow");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("karuselli");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
